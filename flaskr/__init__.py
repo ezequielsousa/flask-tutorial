d29cd93cb51998824420f8dedbfdd7e1330300fb
ezequielsousa <ezequielsousa18@yahoo.com.br>
2022-10-03 23:21:32 -0300

    novo commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    # create and configure the app
+    # cria e configura o aplicativo
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -12,27 +12,27 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
+        # carrega a configuração da instância, se existir, quando não estiver testando
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
+        # carrega a configuração de teste se passado em
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
+    # verifique se a pasta da instância existe
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
+    # uma página simples que diz olá
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
     from . import db
     db.init_app(app)
-
-    return app
 
     from . import auth
     app.register_blueprint(auth.bp)
 
     return app
-
-
-    
